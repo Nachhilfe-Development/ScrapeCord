67a3bb922e481e26885c051b34d1638d1fe772d4
JL710 <76447362+JL710@users.noreply.github.com>
2023-10-19 00:07:47 +0200

    move utils into a class

--- src/renderer/__init__.py
+++ src/renderer/__init__.py
@@ -3,18 +3,25 @@ from pathlib import Path
 from datetime import datetime
 
 
-def render_to_html(data: dict) -> str:
+class Utils:
+    def __init__(self, data: dict):
+        self.__data = data
+
+    @staticmethod
     def format_timestamp(timestamp: float) -> str:
         return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
+    @staticmethod
     def int_color_to_hey(color: int | None) -> str:
         if not color:
             return "#007BF7"
         return f"#{color:06x}"
 
+    @staticmethod
     def filter_ui_components(components: list[dict], action_row: bool = False) -> list[dict]:
         return list(filter(lambda c: c["type"] == "action_row" and action_row, components))
 
+    @staticmethod
     def convert_button_type_to_css_class(button_type: int) -> str:
         return {
             1: "ui_button_primary",
@@ -23,6 +30,8 @@ def render_to_html(data: dict) -> str:
             4: "ui_button_danger",
             5: "ui_button_link",
         }[button_type]
+
+def render_to_html(data: dict) -> str:
 
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(Path(__file__).parent / "templates"),
@@ -34,12 +43,6 @@ def render_to_html(data: dict) -> str:
     return template.render(
         {
             **data,
-            "utils": {
-                "format_timestamp": format_timestamp,
-                "int_color_to_hey": int_color_to_hey,
-                "filter_ui_components": filter_ui_components,
-                "convert_button_type_to_css_class": convert_button_type_to_css_class
-            },
-        }
-
+            "utils": Utils(data),
+        },
     )
